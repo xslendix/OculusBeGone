b7a61c71c53bd5525cc236e2fa6d97f04d5f02ee
xSlendiX <gamingxslendix@gmail.com>
2022-04-26 02:08:06 +0300

    Small installer improvements

--- install.ps1
+++ install.ps1
@@ -10,7 +10,6 @@ function Test-Administrator
     }
 }
 
-
 if (-not (Test-Administrator)) {
     Write-Host "This script requires elevated privileges to run."
     exit 1
@@ -19,16 +18,15 @@ if (-not (Test-Administrator)) {
 # Kill OVRService service
 $ovr = Get-Process OVRServiceLauncher -ErrorAction SilentlyContinue
 if ($ovr) {
+    Write-Host "Stopping OVRService"
     Stop-Process -Name OVRServiceLauncher -Force
 }
-
-# Detect oculus install directory
-oculus_dir=$(Get-ChildItem -Path "C:\Program Files\Oculus" | Where-Object {$_.Name -eq "Oculus"} | Select-Object -ExpandProperty Name)
 
 # Backup OculusDash.exe
 if (Test-Path -Path "C:\Program Files\Oculus\OculusDash.exe") {
     $backup_file = "C:\Program Files\Oculus\OculusDash.exe.backup"
     if (!(Test-Path $backup_file)) {
+        Write-Host "Backing up existing OculusDash.exe"
         New-Item -Path $backup_file
         Copy-Item -Path "C:\Program Files\Oculus\OculusDash.exe" -Destination $backup_file
     }
@@ -36,16 +34,22 @@ if (Test-Path -Path "C:\Program Files\Oculus\OculusDash.exe") {
 
 # Check if new OculusDash.exe exists in the same directory as the script
 if (Test-Path -Path "OculusDash.exe") {
+    Write-Host "Copying new OculusDash.exe"
     Copy-Item -Path "OculusDash.exe" -Destination "C:\Program Files\Oculus"
 } else {
     Write-Host "OculusDash.exe not found in the same directory as the script. Please copy it to the Oculus directory manually."
-}
 
-# Wait for any key press
-Write-Host 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+    # Wait for any key press
+    Write-Host 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
 
 # Start OVRService service
 if ($ovr) {
+    Write-Host "Starting OVRService"
     Start-Process OVRServiceLauncher -ArgumentList -start
 }
+
+# Wait for any key press
+Write-Host 'Successfully installed! Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
